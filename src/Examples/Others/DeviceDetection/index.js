1521b964e4bbce7fb3008de82ee68dc9e80278d3
keenwang <keenwang@zego.im>
2022-03-29 16:05:09 +0800

    ✨ 2.14.0 及以前版本获取媒体设备接口示例

--- src/Examples/Others/DeviceDetection/index.js
+++ src/Examples/Others/DeviceDetection/index.js
@@ -5,12 +5,14 @@ const deviceTypes = ["camera", "microphone", "speaker"]
 let zg = new ZegoExpressEngine(appID, server);
 
 /**
- * get media devices
+ * Get media devices since SDK version 2.14.0
  */
 async function checkDeviceConnection() {
   detectMicrophone(false)
   detectCamera(false)
   detectSpeaker(false)
+
+  // display microphones
   zg.getMicrophones().then(list => {
     microphoneList = list
     const options = microphoneList.map((item, index) => {
@@ -29,6 +31,7 @@ async function checkDeviceConnection() {
     $('#conn-mic').show()
   })
 
+  // display cameras
   zg.getCameras().then(list => {
     cameraList = list
     const options = cameraList.map((item, index) => {
@@ -47,6 +50,7 @@ async function checkDeviceConnection() {
     $('#conn-camera').show()
   })
 
+  // display speakers
   zg.getSpeakers().then(list => {
     speakerList = list
     const options = speakerList.map((item, index) => {
@@ -66,11 +70,84 @@ async function checkDeviceConnection() {
   })
 }
 
+/**
+ * Get media devices before SDK version 2.14.0
+ */
+async function checkDeviceConnectionV2() {
+  detectMicrophone(false)
+  detectCamera(false)
+  detectSpeaker(false)
+  // get media device permissions
+  try {
+    const stream = await zg.createStream({ camera: { video: true, audio: true } });
+    zg.destroyStream(stream);
+  } catch (error) {
+    microphoneList = []
+    $('#select-mic').hide()
+    $('#conn-mic').show()
+    cameraList = []
+    $('#select-camera').hide()
+    $('#conn-camera').show()
+    speakerList = []
+    $('#select-speaker').hide()
+    $('#conn-speaker').show()
+    throw error
+  }
+  // enum devices
+  zg.enumDevices().then((devices) => {
+    // display microphones
+    microphoneList = devices.microphones
+    let options = microphoneList.map((item, index) => {
+      if (!item.deviceName) {
+        item.deviceName = 'microphone' + index;
+      }
+      return ' <option value="' + item.deviceID + '">' + item.deviceName + '</option>';
+    });
+    if (microphoneList.length > 0) {
+      $('#select-mic').html(options.join('')).show();
+      $('#conn-mic').hide()
+    } else {
+      $('#select-mic').hide()
+      $('#conn-mic').show()
+    }
+    // display cameras
+    cameraList = devices.cameras
+    options = cameraList.map((item, index) => {
+      if (!item.deviceName) {
+        item.deviceName = 'cameraList' + index;
+      }
+      return ' <option value="' + item.deviceID + '">' + item.deviceName + '</option>';
+    });
+    if (cameraList.length > 0) {
+      $('#select-camera').html(options.join('')).show();
+      $('#conn-camera').hide()
+    } else {
+      $('#select-camera').hide()
+      $('#conn-camera').show()
+    }
+    // display speakers
+    speakerList = devices.speakers
+    options = speakerList.map((item, index) => {
+      if (!item.deviceName) {
+        item.deviceName = 'speakerList' + index;
+      }
+      return ' <option value="' + item.deviceID + '">' + item.deviceName + '</option>';
+    });
+    if (speakerList.length > 0) {
+      $('#select-speaker').html(options.join('')).show();
+      $('#conn-speaker').hide()
+    } else {
+      $('#select-speaker').hide()
+      $('#conn-speaker').show()
+    }
+  })
+}
+
 
 const detectMicrophone = (() => {
   micStream = null
-
   return async (detect = true) => {
+    detect ? $("#result-microphone").show() : $("#result-microphone").hide()
     if (micStream) {
       zg.destroyStream(micStream)
       micStream = null
@@ -99,6 +176,7 @@ const detectMicrophone = (() => {
 const detectCamera = (() => {
   stream = null
   return async (detect = true) => {
+    detect ? $("#result-camera").show() : $("#result-camera").hide()
     if (stream) {
       zg.destroyStream(stream)
       stream = null
@@ -125,7 +203,7 @@ const detectCamera = (() => {
 
 function detectSpeaker(detect = true) {
   const player = document.querySelector("#mic-player")
-
+  detect ? $("#result-speaker").show() : $("#result-speaker").hide()
   if (!detect) {
     player?.pause()
     return
@@ -133,7 +211,7 @@ function detectSpeaker(detect = true) {
   const deviceID = $("#select-speaker").val()
   if (deviceID !== undefined) {
     try {
-      // `setAudioOutputDevice` support since version 2.15.0
+      // `setAudioOutputDevice`  is supported since version 2.15.0
       zg.setAudioOutputDevice(player, deviceID)
       player?.play()
     } catch (error) {
@@ -143,13 +221,6 @@ function detectSpeaker(detect = true) {
 }
 
 function detectDevice(type) {
-  deviceTypes.forEach(item => {
-    if (item === type) {
-      $(`#result-${item}`).show()
-    } else {
-      $(`#result-${item}`).hide()
-    }
-  })
   if (type === "camera") {
     detectCamera()
     detectMicrophone(false)
@@ -173,9 +244,6 @@ function main() {
   // bind SDK events
   zg.on("capturedSoundLevelUpdate", (level) => {
     $("#mic-progress").val(level)
-  })
-  deviceTypes.forEach(item => {
-      $(`#result-${item}`).hide()
   })
   checkDeviceConnection()
 }

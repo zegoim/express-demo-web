e0fb967713832c4da5c386970a37f4223a6e7e03
yytm <yytm0112@163.com>
2023-09-27 18:43:06 +0800

    ✨ 增加本地导播

--- src/assets/js/left-menu.js
+++ src/assets/js/left-menu.js
@@ -118,6 +118,9 @@ const menu = `
         <a href="${baseURL}/Others/VirtualBackground/index.html" class="waves-effect" data-lang="VirtualBackground">虚拟背景</a>
       </li>
       <li>
+        <a href="${baseURL}/Others/StreamCompositor/index.html" class="waves-effect" data-lang="StreamCompositor">本地导播</a>
+      </li>
+      <li>
         <a href="${baseURL}/Others/EffectsBeauty/index.html" class="waves-effect" data-lang="BaseBeauty">基础美颜</a>
       </li>
       <span class="sidebar-title" data-lang="DebugAndConfig">调试与配置</span>
